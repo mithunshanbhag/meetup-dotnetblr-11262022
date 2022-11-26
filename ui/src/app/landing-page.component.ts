2aefc5fd7c0e47855f0118f3a8b8225be7294dd7
mithun shanbhag <mithunshanbhag@hotmail.com>
2022-11-26 13:04:23 +0530

    display username + api response

--- ui/src/app/landing-page.component.ts
+++ ui/src/app/landing-page.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { MsalBroadcastService, MsalService } from '@azure/msal-angular';
-import { Observable } from 'rxjs';
+import { MsalService } from '@azure/msal-angular';
+import { Observable, of } from 'rxjs';
 import { Constants } from './constants';
 
 @Component({
@@ -9,8 +9,8 @@ import { Constants } from './constants';
   template: `
     <div style="text-align:center">
     <h1>{{title}}</h1>
-    <p>welcome {{userName$ | async}}</p>
-    <img src="{{ userPic$ | async}}"/>
+    <p><b>User:</b> {{userName$ | async}}</p>
+    <p><b>API Response:</b> {{apiResponse$ | async}}</p>
     </div>
   `,
   styles: []
@@ -19,8 +19,9 @@ export class LandingPageComponent implements OnInit {
 
   title = 'demo: implicit flow (landing page)';
 
+  apiResponse$: Observable<any> = new Observable<any>();
+
   userName$: Observable<string> = new Observable<string>();
-  userPic$: Observable<string> = new Observable<string>();
 
   constructor(
     private msalService: MsalService,
@@ -34,12 +35,17 @@ export class LandingPageComponent implements OnInit {
 
     // dump user info to console
     this.msalService.instance.getAllAccounts().forEach(account => {
+      this.userName$ = of(account.name) as Observable<string>;
       console.log(`name: ${account.name}`);
       console.log(`username: ${account.username}`);
       console.log(`tenantId: ${account.tenantId}`);
     });
 
     // call items API
-    this.httpClient.get<string>(`${Constants.apiEndpointUri}/items/1`).subscribe();
+    this.httpClient.get(`${Constants.apiEndpointUri}/items/1`, { responseType: 'text' })
+      .subscribe(result => {
+        this.apiResponse$ = of(result);
+        console.log(`api response: ${result}`);
+      });
   }
 }
